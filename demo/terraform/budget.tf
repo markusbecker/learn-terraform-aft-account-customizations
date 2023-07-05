dcf110739a749cd5bb178e57c64b2191ac1dc6d0
Markus Becker <markus.becker@cloud-plant.com>
2023-07-05 16:01:23 +0200

    Changed default customization for demo

--- demo/terraform/budget.tf
+++ demo/terraform/budget.tf
@@ -1,9 +1,19 @@
-resource "aws_budgets_budget" "total_cost" {
-  name              = "budget-total-monthly"
-  budget_type       = "COST"
-  limit_amount      = "100"
-  limit_unit        = "USD"
-  time_period_end   = "2025-06-15_00:00"
-  time_period_start = "2023-06-06_00:00"
-  time_unit         = "MONTHLY"
+# resource "aws_budgets_budget" "total_cost" {
+#   name              = "budget-total-monthly"
+#   budget_type       = "COST"
+#   limit_amount      = "100"
+#   limit_unit        = "USD"
+#   time_period_end   = "2025-06-15_00:00"
+#   time_period_start = "2023-06-06_00:00"
+#   time_unit         = "MONTHLY"
+# }
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "first" {
+  bucket = "my-tf-test-${data.aws_caller_identity.current.account_id}"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
